04be0109957f128af35f80d22bb940fd16fa80c4
Cooper Lees <me@cooperlees.com>
2020-11-26 18:38:35 -0800

    Remove timestamp from guage statistics

--- src/firestarter/prometheus.py
+++ src/firestarter/prometheus.py
@@ -3,30 +3,26 @@
 import asyncio
 from aiohttp import web
 from socket import getfqdn
-from time import time
 
 
 PROMETHEUS_TEMPLATE = """\
 # HELP fireplace_status Is the fireplace running (bool)
 # TYPE fireplace_status gauge
-fireplace_status{hostname="%s"} %i %i
+fireplace_status{hostname="%s"} %i
 
 # HELP fireplace_coroutines Number of current running asyncio coroutines
 # TYPE fireplace_coroutines gauge
-fireplace_coroutines{hostname="%s"} %i %i
+fireplace_coroutines{hostname="%s"} %i
 """
 
 
 async def stats(request: web.Request) -> web.Response:
     hostname = getfqdn()
     state = await request.app["fireplace"].lit()
-    stat_time = int(time())
     formatted_metrics = PROMETHEUS_TEMPLATE % (
         hostname,
-        stat_time,
         int(state),
         hostname,
-        stat_time,
         len(asyncio.all_tasks()),
     )
     return web.Response(text=formatted_metrics)
